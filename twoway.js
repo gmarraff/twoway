fc33536fe49694088332d68bb344f8eeb2ca40b8
= <=>
2016-10-25 00:49:27 +0200

    Improved radio handler and added array serializer, wrote in a more standard way

--- twoway.js
+++ twoway.js
@@ -1,216 +1,216 @@
-function TwoWay(hash, label){
-    this.coalesce = function(val, sub){
-        if(val === undefined || val === null)
+var TwoWay = (function(){
+    var hash;
+    var label;
+    var coalesce = function (val, sub){
+        if (val === undefined || val === null)
             return sub;
         return val;
     };
-    this.DOMcoalesce = function(val, sub){
-        if(val === undefined || val === null || val === '')
+    var DOMcoalesce = function(val, sub) {
+        if (val === undefined || val === null || val === '')
             return sub;
         return val;
     };
-    this.hash = this.coalesce(hash, {});
-    this.label = this.coalesce(label, "data-twoway_label");
+    var TwoWay = function(hash, label){
+        hash = coalesce(hash, {});
+        label = coalesce(label, "data-twoway_label");
+    };
 
-    this.setHash = function(hash){
-        this.hash = hash;
-    };
-    this.getHash = function(){
-        return this.hash;
-    };
-    //tested
-    this.radioValue = function(obj){
-        if(typeof(obj) != 'object')
-            obj = $('input[data-twoway_label="'+ obj +'"]:checked');
-        var options = this.coalesce($(obj).attr('data-twoway_options'), '');
+    var radioValue = function (obj) {
+        if (typeof(obj) != 'object')
+            obj = $('input[data-twoway_label="' + obj + '"]:checked');
+        var options = coalesce($(obj).attr('data-twoway_options'), '');
         var value = $(obj).val();
-        if(options.search('convert_boolean') != -1)
+        if (options.search('convert_boolean') != -1)
             value = (value == 'true');
         return value;
     };
-
-    //tested
-    this.checkboxValue = function(obj){
-      var bool_as = $(obj).attr('data-twoway_bool_as');
-      var checked = $(obj).prop('checked');
-      if(bool_as == undefined || bool_as == null)
-        return (checked ? $(obj).val() : '');
-      else if(bool_as == 'true')
-        return checked;
-      else
-        return !checked;
+    var checkboxValue = function (obj) {
+        var bool_as = $(obj).attr('data-twoway_bool_as');
+        var checked = $(obj).prop('checked');
+        if (bool_as == undefined || bool_as == null)
+            return (checked ? $(obj).val() : '');
+        else if (bool_as == 'true')
+            return checked;
+        else
+            return !checked;
     };
-    this.multipleValues = function(label, delimiter, typology){
-      var _this = this;
-      var values = [];
-      typology = _this.coalesce(typology, '');
-      $(':input[data-twoway_label="' + label +'"]' + typology).each(function(index, value){
-        var value = (typology === '' ? _this.checkboxValue(this) : _this.radioValue(this));
-        var options = _this.coalesce($(this).attr('data-twoway_options'), '');
-        var include_blank = options.search('reject_blank') == -1;
-        if(value !== '' || (value === '' && include_blank)) {
-            values.push(value);
-        }
-      });
-      if(delimiter !== 'array') values = values.join(delimiter);
-      return values;
+    var multipleValues = function (label, delimiter, typology) {
+        var values = [];
+        typology = coalesce(typology, '');
+        $(':input[data-twoway_label="' + label + '"]' + typology).each(function (index, value) {
+            var value = (typology === '' ? checkboxValue(this) : radioValue(this));
+            var options = coalesce($(this).attr('data-twoway_options'), '');
+            var include_blank = options.search('reject_blank') == -1;
+            if (value !== '' || (value === '' && include_blank)) {
+                values.push(value);
+            }
+        });
+        if (delimiter !== 'array') values = values.join(delimiter);
+        return values;
     };
-    //tested
-    this.inputValue = function(obj){
+    var inputValue = function (obj) {
         return $(obj).val();
     };
-
-    //tested
-    this.fillHash = function(hash, hash_div, value, index){
-        if(hash_div[index] === undefined)
+    var fillHash = function (hash, hash_div, value, index) {
+        if (hash_div[index] === undefined)
             return;
-        if(hash[hash_div[index]] === undefined)
+        if (hash[hash_div[index]] === undefined)
             return;
-        if((index + 1) == hash_div.length)
+        if ((index + 1) == hash_div.length)
             hash[hash_div[index]] = value;
         else
-            this.fillHash(hash[hash_div[index]], hash_div, value, index + 1);
+            fillHash(hash[hash_div[index]], hash_div, value, index + 1);
     };
-
-    //tested
-    this.setValueToSpan = function(value, label){
-        $('span[data-twoway_label="'+ label +'"]').each(function(){
+    var setValueToSpan = function (value, label) {
+        $('span[data-twoway_label="' + label + '"]').each(function () {
             $(this).text(value);
         });
     };
-
-    //tested
-    this.setCheck = function(obj, value){
-        if($(obj).val() == String(value))
-            $(obj).prop('checked', true);
-    };
-
-    //is ok
-    this.setGenericInput = function(obj, value){
-        $(obj).val(value);
-    };
-
-    //tested
-    this.setValueToInput = function(obj, value){
-        switch($(obj).attr('type')){
-            case 'radio':
-                this.setCheck(obj, value);
-                break;
-            case 'checkbox':
-                this.setCheck(obj, value);
-                break;
-            default:
-                value = this.setGenericInput(obj, value);
-        }
+    var TwTrigger = function(obj){ //function because i'm planning to trigger more events
         $(obj).trigger('change');
     };
-
-    //tested
-    this.setValue = function(value, label){
-        var hash_div = label.split('|');
-        if(hash_div.length > 0) {
-            this.fillHash(this.hash, hash_div, value, 0);
-            this.setValueToSpan(value, label);
+    var setRadio = function(obj, value){
+        if ($(obj).val() == String(value)) {
+            $(obj).prop('checked', true);
+            TwTrigger(obj);
         }
     };
-
-    //tested
-    this.getValue = function(obj){
+    var setCheck = function (obj, value) {
+        if ($(obj).val() == String(value))
+            $(obj).prop('checked', true);
+        TwTrigger(obj);
+    };
+    var setGenericInput = function (obj, value) {
+        $(obj).val(String(value));
+        TwTrigger(obj);
+    };
+    var setValueToInput = function (obj, value) {
+        switch ($(obj).attr('type')) {
+            case 'radio':
+                setRadio(obj, value);
+                break;
+            case 'checkbox':
+                setCheck(obj, value);
+                break;
+            default:
+                value = setGenericInput(obj, value);
+        }
+    };
+    var setValue = function (value, label) {
+        var hash_div = label.split('|');
+        if (hash_div.length > 0) {
+            fillHash(hash, hash_div, value, 0);
+            setValueToSpan(value, label);
+        }
+    };
+    var getValue = function (obj) {
         var value;
         var label = $(obj).attr('data-twoway_label');
         var delimiter = $(obj).attr('data-twoway_multiple');
-        switch($(obj).attr('type')){
+        switch ($(obj).attr('type')) {
             case 'radio':
-                if(delimiter === null || delimiter == undefined)
-                    value = this.radioValue(label);
+                if (delimiter === null || delimiter == undefined)
+                    value = radioValue(label);
                 else
-                    value = this.multipleValues(label, delimiter, ':checked');
+                    value = multipleValues(label, delimiter, ':checked');
                 break;
             case 'checkbox':
-                if(delimiter === null || delimiter === undefined)
-                  value = this.checkboxValue(obj);
+                if (delimiter === null || delimiter === undefined)
+                    value = checkboxValue(obj);
                 else
-                  value = this.multipleValues(label, delimiter);
+                    value = multipleValues(label, delimiter);
                 break;
             default:
-                value = this.inputValue(obj);
+                value = inputValue(obj);
         }
         return value;
-
     };
-
-    //tested
-    this.bind = function(obj){
-        var label = $(obj).attr('data-twoway_label');
-        var value = this.getValue(obj);
-        this.setValue(value, label);
-    };
-
-    //tested
-    this.fetchInput = function(label, val){
-        var _this = this;
-        $('input[data-twoway_label="' + label +'"]').each(function(index, obj){
-            _this.setValueToInput(obj, val)
+    var fetchInput = function (label, val) {
+        $('input[data-twoway_label="' + label + '"]').each(function (index, obj) {
+            setValueToInput(obj, val)
         });
     };
-
-    //tested
-    this.browseHash = function(hash, label){
-        var _this = this;
-        $.each(hash, function(key, val){
+    var browseHash = function (hash, label) {
+        $.each(hash, function (key, val) {
             var actual_label = label + key + '|';
-            val = _this.coalesce(val, '');
-            if(typeof(val) == 'object')
-                _this.browseHash(val, actual_label);
+            val = coalesce(val, '');
+            if (typeof(val) == 'object')
+                browseHash(val, actual_label);
             else {
                 actual_label = actual_label.slice(0, -1);
-                _this.fetchInput(actual_label, val);
-                _this.setValueToSpan(val, actual_label);
+                fetchInput(actual_label, val);
+                setValueToSpan(val, actual_label);
             }
         });
     };
-    //tested
-    this.initializeFields = function(){
-        this.browseHash(this.hash, '');
+    var removeField = function (field_path) { //this method will be private
+        if (field_path[field_path.length - 1] == "|") field_path = field_path.slice(0, -1);
+        var dyingElement = 'hash';
+        $.each(field_path.split('|'), function (index, field) {
+            dyingElement += '["' + field + '"]';
+        });
+        eval("delete " + dyingElement);
     };
 
-    //tested
-    this.build = function(main_field, options){
-        var _this = this;
-        main_field = _this.coalesce(main_field, 'object');
-        if(main_field[main_field.length - 1] == "|") main_field_path = main_field.slice(0, -1);
-        options = _this.coalesce(options, {bind: false, from_zero: false});
-        options['bind'] = _this.coalesce(options['bind'], false);
-        options['from_zero'] = _this.coalesce(options['from_zero'], false);
-        if(options['blank_value'] === undefined) options['blank_value'] = '';
-        if(options['from_zero']) _this.hash = {};
-        var building = _this.hash;
-        $(":input[data-twoway_label*='" + main_field + "|']").each(function(index, element){
+    //PUBLIC
+    TwoWay.prototype.setHash = function (newHash) {
+        hash = coalesce(newHash, {});
+    };
+    TwoWay.prototype.getHash = function () {
+        return hash;
+    };
+    TwoWay.prototype.getPart = function(label){
+        label = coalesce(label, '').split('|');
+        var part = hash;
+        $.each(label, function(index, field){
+            part = part[field];
+        });
+        return part;
+    };
+    TwoWay.prototype.serializePart = function(label){
+        var array = [];
+        $.each(this.getPart(label), function(index, el){
+            array.push(el);
+        });
+        return array;
+    };
+    TwoWay.prototype.initializeFields = function () {
+        browseHash(hash, '');
+    };
+    TwoWay.prototype.bind = function (obj) {
+        var label = $(obj).attr('data-twoway_label');
+        var value = getValue(obj);
+        setValue(value, label);
+    };
+    TwoWay.prototype.build = function (main_field, options) {
+        main_field = coalesce(main_field, 'object');
+        if (main_field[main_field.length - 1] == "|") main_field = main_field.slice(0, -1);
+        options = coalesce(options, {bind: false, from_zero: false});
+        options['bind'] = coalesce(options['bind'], false);
+        options['from_zero'] = coalesce(options['from_zero'], false);
+        if (options['blank_value'] === undefined) options['blank_value'] = ''; //default value can be null
+        if (options['from_zero']) hash = {};
+        var building = hash;
+        $(":input[data-twoway_label*='" + main_field + "|']").each(function (index, element) {
             var field = $(element).attr('data-twoway_label');
             var attributes = field.split('|');
-            $.each(attributes, function(index, attribute){
-                if(index == (attributes.length - 1))
-                    building[attribute] = (options.bind ? _this.DOMcoalesce(_this.getValue(element), options.blank_value) : options.blank_value);
+            $.each(attributes, function (index, attribute) {
+                if (index == (attributes.length - 1))
+                    building[attribute] = (options.bind ? DOMcoalesce(getValue(element), options.blank_value) : options.blank_value);
                 else
-                    building[attribute] = _this.coalesce(building[attribute], {});
+                    building[attribute] = coalesce(building[attribute], {});
                 building = building[attribute];
             });
-            building = _this.hash;
+            building = hash;
         });
-        this.removeField = function(field_path){ //this method will be private
-          if(field_path[field_path.length - 1] == "|") field_path = field_path.slice(0, -1);
-          var dyingElement = 'this.hash';
-          $.each(field_path.split('|'), function(index, field){
-            dyingElement += '["' + field + '"]';
-          });
-          eval("delete " + dyingElement);
-        }
-        this.softRemove = function(field_path){
-          this.removeField(field_path);
-        }
-        this.remove = function(field_path){
-          $('[data-twoway_label*="' + field_path + '"').remove();
-          this.removeField(field_path);
-        }
-    }
-}
+    };
+    TwoWay.prototype.softRemove = function (field_path) {
+        removeField(field_path);
+    };
+    TwoWay.prototype.remove = function (field_path) {
+        $('[data-twoway_label*="' + field_path + '"').remove();
+        removeField(field_path);
+    };
+    return TwoWay;
+})();
